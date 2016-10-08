5d4703272d4e4dee61c94b0d012f37dc1a211f10
ViralPanda <patel.2004@osu.edu>
2016-10-08 10:11:13 -0400

    Working post server

--- server/server.js.js
+++ server/server.js.js
@@ -1,14 +1,35 @@
 var express = require('express');
+var bodyparser = require('body-parser');
 var app = express();
-var fs = require("fs");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+var watson = require('watson-developer-cloud');
+var alchemy_language = watson.alchemy_language({
+  api_key: 'bc5336851577a2cf1e01df99007fd2a538ef6298'
+})
 
 
-app.get('/user/1', function (req, res) {
-	console.log("Request received " + req);
-	
-	//send new request
+app.post('/user/1', function (request, res) {
+	request.on('data', function(chunk) {
+		var body = [];
+		body.push(chunk);
+		var parameters = {
+			text: Buffer.concat(body).toString()
+		}
+		alchemy_language.sentiment(parameters, function (err, response) {
+		if (err)
+			console.log('error:', err);
+		else
+			res.a
+			console.log(JSON.stringify(response, null, 2));
+		});
+		console.log("Request received " + JSON.stringify(parameters.text));
+		
+	});
    res.end("Access successful");
 })
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
@@ -16,3 +37,4 @@ var server = app.listen(8081, function () {
 
    console.log("Example app listening at http://%s:%s", host, port)
 })
+
