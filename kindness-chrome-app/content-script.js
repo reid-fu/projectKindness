eb54c242bd0007ee217ebe6e28e892acd1de9478
Benjamin Stammen <benjamin@stammen.me>
2016-10-08 13:33:28 -0400

    dynamically added textareas work

--- kindness-chrome-app/content-script.js
+++ kindness-chrome-app/content-script.js
@@ -1,32 +1,40 @@
 $(document).ready(function() {
-  console.log("Page Ready!");
-  $(document).live("onchange",function() {
-    console.log("Page Changed!");
-    findTextAreas();
+  console.log("page ready.");
+  findTextAreas();
+
+  $(document).on("DOMNodeInserted", function(e) {
+    if ($(e.target).is("textarea")) {
+      console.log("new textarea available");
+       setupTextArea(e.target);
+    }
   });
 });
 
 function findTextAreas() {
   // Add an icon to all input fields of class text
+  console.log("finding textareas");
   var textAreas = document.getElementsByTagName("textarea");
   for (var i = 0; i < textAreas.length; i++) {
-    var textArea = textAreas[i];
-    if (textArea.readOnly == false) {
-      // var elem = document.createElement("img");
-      // elem.setAttribute("src", "icon.png");
-      // inputField.parentElement.appendChild(elem);
-      var oldParent = textArea.parentElement;
-      var newContainer = document.createElement("div");
-      var toolbar = document.createElement("div");
+    setupTextArea(textAreas[i]);
+  }
+}
 
-      oldParent.insertBefore(newContainer, textArea);
-      newContainer.appendChild(toolbar);
-      newContainer.appendChild(textArea);
+function setupTextArea(textArea) {
+  if (textArea.readOnly == false) {
+    // var elem = document.createElement("img");
+    // elem.setAttribute("src", "icon.png");
+    // inputField.parentElement.appendChild(elem);
+    var oldParent = textArea.parentElement;
+    var newContainer = document.createElement("div");
+    var toolbar = document.createElement("div");
 
-      // We have to check for modifications to these fields
-      textArea.oninput = inputChanged();
-      inputField.onpropertychange = inputField.oninput;
-    }
+    oldParent.insertBefore(newContainer, textArea);
+    newContainer.appendChild(toolbar);
+    newContainer.appendChild(textArea);
+
+    // We have to check for modifications to these fields
+    textArea.oninput = inputChanged();
+    textArea.onpropertychange = textArea.oninput;
   }
 }
 
@@ -35,5 +43,5 @@ function inputChanged() {
 }
 
 function sendInputForFeedback() {
-  
+
 }
