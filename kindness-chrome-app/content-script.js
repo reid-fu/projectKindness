0e2aed6d6e66f6d1faa77b39e054d8fad1faa2e6
Benjamin Stammen <benjamin@stammen.me>
2016-10-08 11:52:40 -0400

    trying to get dynamically added text areas to work

--- kindness-chrome-app/content-script.js
+++ kindness-chrome-app/content-script.js
@@ -1,16 +1,39 @@
-// Add an icon to all input fields of class text
-var inputFields = document.getElementsByTagName("input");
-for (var i = 0; i < inputFields.length; i++) {
-  var inputField = inputFields[i];
-  if (inputField.getAttribute("type") == "text") {
-    var elem = document.createElement("img");
-    elem.setAttribute("src", "icon.png");
-    inputField.parentElement.appendChild(elem);
+$(document).ready(function() {
+  console.log("Page Ready!");
+  $(document).live("onchange",function() {
+    console.log("Page Changed!");
+    findTextAreas();
+  });
+});
 
-    // We have to check for modifications to these fields
-    inputField.oninput = function() {
-      console.log("new text for analysis");
-    };
-    inputField.onpropertychange = inputField.oninput;
+function findTextAreas() {
+  // Add an icon to all input fields of class text
+  var textAreas = document.getElementsByTagName("textarea");
+  for (var i = 0; i < textAreas.length; i++) {
+    var textArea = textAreas[i];
+    if (textArea.readOnly == false) {
+      // var elem = document.createElement("img");
+      // elem.setAttribute("src", "icon.png");
+      // inputField.parentElement.appendChild(elem);
+      var oldParent = textArea.parentElement;
+      var newContainer = document.createElement("div");
+      var toolbar = document.createElement("div");
+
+      oldParent.insertBefore(newContainer, textArea);
+      newContainer.appendChild(toolbar);
+      newContainer.appendChild(textArea);
+
+      // We have to check for modifications to these fields
+      textArea.oninput = inputChanged();
+      inputField.onpropertychange = inputField.oninput;
+    }
   }
+}
+
+function inputChanged() {
+  console.log("new text for analysis");
+}
+
+function sendInputForFeedback() {
+  
 }
